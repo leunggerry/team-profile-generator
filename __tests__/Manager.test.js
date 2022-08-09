85a044bd97592a6183d05b7170caf00b50efc0ba
gigi.upNorth <leung.gerry@gmail.com>
2022-08-09 00:20:05 -0400

    [Manager] Adding Manager class that extends the employee

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,18 @@
+/** Required Libraries
+ **************************************************************************************************/
+const Manager = require("../lib/Manager");
+/** Mocks
+ *************************************************************************************************/
+
+/** Tests
+ *************************************************************************************************/
+test("Check manager attributes", () => {
+  //setup
+  const manager = new Manager("Tom", 1, "tom@mail.com", 3);
+
+  //validate
+  expect(manager.name).toBe("Tom");
+  expect(manager.id).toBe(1);
+  expect(manager.email).toBe("tom@mail.com");
+  expect(manager.officeNumber).toBe(3);
+});
