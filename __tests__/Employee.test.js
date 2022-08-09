5f75f296c6db28602dce655d4c4b4f8b8a797708
gigi.upNorth <leung.gerry@gmail.com>
2022-08-08 23:46:03 -0400

    [Employee] Adding Employee attributes and methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -16,3 +16,27 @@ test("Check Employee Credentials", () => {
   expect(employee.id).toBe(1);
   expect(employee.email).toBe("tom@mail.com");
 });
+
+test("Check Employee Role", () => {
+  // Setup
+  const employee = new Employee("Tom", 1, "tom@mail.com");
+
+  // Verify
+  expect(employee.getRole()).toBe("Employee");
+});
+
+test("Check Employee Name", () => {
+  // Setup
+  const employee = new Employee("Tom", 1, "tom@mail.com");
+
+  // Verify
+  expect(employee.getName()).toBe("Tom");
+});
+
+test("Check Employee ID", () => {
+  // Setup
+  const employee = new Employee("Tom", 1, "tom@mail.com");
+
+  // Verify
+  expect(employee.getId()).toBe(1);
+});
