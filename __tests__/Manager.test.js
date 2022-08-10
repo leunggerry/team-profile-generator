40a373836f138c6353be2df86d26e8bdef01262f
gigi.upNorth <leung.gerry@gmail.com>
2022-08-09 21:42:19 -0400

    [Manager] Adding Manager role along with tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -16,3 +16,16 @@ test("Check manager attributes", () => {
   expect(manager.email).toBe("tom@mail.com");
   expect(manager.officeNumber).toBe(3);
 });
+
+test("Check the manager role", () => {
+  //setup
+  const manager = new Manager("Tom", 1, "tom@mail.com", 3);
+
+  //validate
+  expect(manager.name).toBe("Tom");
+  expect(manager.id).toBe(1);
+  expect(manager.email).toBe("tom@mail.com");
+  expect(manager.officeNumber).toBe(3);
+
+  expect(manager.getRole()).toBe("Manager");
+});
