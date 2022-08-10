af480a2ec932d6730f388d0c5f51844e2fcaaa5b
gigi.upNorth <leung.gerry@gmail.com>
2022-08-09 22:33:33 -0400

    [Intern] Adding Intern class with testing the attributes and methods of Intern class

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,28 @@
+/** Required Libraries
+ **************************************************************************************************/
+const Intern = require("../lib/Intern");
+/** Mocks
+ *************************************************************************************************/
+
+/** Tests
+ *************************************************************************************************/
+test("Check the Interns attributes", () => {
+  const intern = new Intern("Tom", 1, "tom@mail.com", "Carleton");
+
+  expect(intern.name).toBe("Tom");
+  expect(intern.id).toBe(1);
+  expect(intern.email).toBe("tom@mail.com");
+  expect(intern.school).toBe("Carleton");
+});
+
+test("Check the School", () => {
+  const intern = new Intern("Tom", 1, "tom@mail.com", "Carleton");
+
+  expect(intern.getSchool()).toBe("Carleton");
+});
+
+test("Check the Role", () => {
+  const intern = new Intern("Tom", 1, "tom@mail.com", "Carleton");
+
+  expect(intern.getRole()).toBe("Intern");
+});
